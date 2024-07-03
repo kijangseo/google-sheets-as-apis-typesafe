8558afa35c536b0f34e1e9ff0e65de811097c7be
Stefan Iordache <stefan.iordache2001@gmail.com>
2024-07-03 12:33:55 +0300

    use placeholder for project name in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { BackendService } from "@genezio-sdk/typesafe-getting-started";
+import { BackendService } from "@genezio-sdk/(•◡•)project-name(•◡•)";
 import "./App.css";
 
 export default function App() {
