4460d3215937bef1c884f41946d7c0fb97455f02
Stefan Iordache <stefan.iordache2001@gmail.com>
2024-07-04 11:38:31 +0300

    Revert "use placeholder for project name in client"
    This reverts commit 8558afa35c536b0f34e1e9ff0e65de811097c7be.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { BackendService } from "@genezio-sdk/(•◡•)project-name(•◡•)";
+import { BackendService } from "@genezio-sdk/typesafe-getting-started";
 import "./App.css";
 
 export default function App() {
